bbf629a081043279e167c6df61c9470d18b54703
computer <computer@admin-nb.local>
2017-01-16 13:00:00 +0700

    Init

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost:33061;dbname=yiiproject',
+    'username' => 'root',
+    'password' => '123456',
+    'charset' => 'utf8',
+];
---
+++ views/site/index.php
@@ -0,0 +1,22 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'My Yii Application';
+?>
+<div class="site-index">
+
+    <div class="jumbotron">
+        <h1>ยินดีต้อนรับสู่ Yii 2</h1>
+
+        <p class="lead">เปิดประตูรับประสบการณ์ใหม่ ๆ</p>
+
+        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
+    </div>
+
+    <div class="body-content">
+
+        
+
+    </div>
+</div>
